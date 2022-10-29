2ac51e52af8f1a7dba1aa52f4aa69e83f6ea2f12
Hoan Đinh <hoan.dinh@southtelecom.vn>
2022-10-29 14:50:26 +0700

    update report

--- resources/views/main/assessment/assessment_result.blade.php
+++ resources/views/main/assessment/assessment_result.blade.php
@@ -37,6 +37,16 @@
                         </h3>
                     </div>
                 </div>
+                <div class="m-portlet__head-tools">
+                    <ul class="m-portlet__nav">
+                        <li class="m-portlet__nav-item">
+                            <a href="{{route('chart_assessment', $assessment->id . '-' . $assessment->slug())}}"
+                                class="m-portlet__nav-link btn btn-primary">
+                                Xem biểu đồ
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class="m-portlet__body">
                 <div class="m-section">
